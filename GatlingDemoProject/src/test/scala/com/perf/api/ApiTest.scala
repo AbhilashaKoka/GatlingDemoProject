5105983e826d3ad5b1c3fbe6930e3ec8d8256f97
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2024-12-29 00:28:25 +0530

    commit 28-12-2024 03

---
+++ GatlingDemoProject/src/test/scala/com/perf/api/ApiTest.scala
@@ -0,0 +1,25 @@
+package com.perf.api
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+
+class ApiTest extends Simulation{
+  //Gatling script we need 3 thing
+  //protocol
+  val httpProtocol=http.baseUrl("https://reqres.in/api/users")
+  //scenario
+  val scn=scenario("Get API for single user")
+    .exec(http("Single user").get("/2")
+      .check(
+       status.is(200),
+      jsonPath("$.data.first_name").is("Janet"))
+            )
+      .pause(1)
+
+  //setup
+  setUp(
+  scn.inject(rampUsers(10).during(5))
+    .protocols(httpProtocol)
+  )
+}
