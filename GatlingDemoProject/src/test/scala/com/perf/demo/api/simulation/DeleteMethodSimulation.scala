f0004053017e089e719ab41b24812396ebd8ef7e
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2024-12-29 23:46:51 +0530

    commit 29-12-2024 02

--- GatlingDemoProject/src/test/scala/com/perf/demo/api/simulation/DeleteMethodSimulation.scala
+++ GatlingDemoProject/src/test/scala/com/perf/demo/api/simulation/DeleteMethodSimulation.scala
@@ -1,3 +1,28 @@
 package com.perf.demo.api.simulation
 
-class DeleteMethodSimulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class DeleteMethodSimulation extends Simulation{
+
+  //protocol
+  val httpProtocol=http
+    .baseUrl("https://reqres.in/api")
+
+  //scenario
+  val deleteuserscn1=scenario("delete User")
+    .exec(
+      http("delete user req")
+        .delete("/users/2")
+        .check(status is 204)
+    )
+        .pause(1)
+
+  //setup
+  setUp(
+    deleteuserscn1.inject(rampUsers(3).during(2)).protocols(httpProtocol)
+
+  )
+
+}
+
