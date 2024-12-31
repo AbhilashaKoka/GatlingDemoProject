cc831bb94eb7e0ad2c773d265d2d5f73a66c342d
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2024-12-31 16:58:16 +0530

    commit 31-12-2024 01

--- GatlingDemoProject/src/test/scala/com/perf/demo/api/simulation/DemoFeeder.scala
+++ GatlingDemoProject/src/test/scala/com/perf/demo/api/simulation/DemoFeeder.scala
@@ -5,19 +5,48 @@ import io.gatling.http.Predef._
 
 class DemoFeeder extends Simulation{
 
+  //protocol
+  val httpProtocol = http.baseUrl("https://reqres.in/api")
+
   val feeder=csv("testDataFeeders/user.csv")
 
-  val scn=scenario("Feeder Demo")
-    .repeat(3) {
-    feed(feeder)
-        .exec { session =>
-          println("name:" + session("name").as[String])
-          println("job:" + session("job").as[String])
-          session
-        }
-        .pause(1)
-    }
+ // val scn=scenario("Feeder Demo")
+//    .repeat(3)
+//    {
+//    feed(feeder)
+//        .exec { session =>
+//          println("name:" + session("name").as[String])
+//          println("job:" + session("job").as[String])
+//          session
+//        }
+//        .pause(1)
+//
 
-  setUp(scn.inject(atOnceUsers(1)))
+  val scn = scenario("Create User")
+    .feed(feeder)
+    .exec(
+      http("Create user req")
+        .post("/users")
+        .header("content-type", "application/json")
+        .asJson
+        .body(StringBody(
+          """
+            |{
+            |  "name": "${name}",
+            |  "job": "${job}"
+            |}
+            |""".stripMargin)).asJson
+//.body(RawFileBody("testDataFeeders/user.json")).asJson
+        .check(
+          status is 201,
+          jsonPath("$.name") is "morpheus"))
+
+  //setup
+  setUp(
+    scn.inject(rampUsers(3).during(2)).protocols(httpProtocol))
 
 }
+
+
+
+
